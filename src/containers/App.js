fca40861fb59143f311baeb1f848e21d516a3530
christine M bruton <christinembruton@christines-MacBook-Air.local>
2020-09-04 21:22:41 -0700

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+
+class App extends Component { //smart component
+    constructor(){
+        super() //calls the constructor of the component
+        this.state = { //describes the app, somethign that can change and describe the app
+            robots: [], 
+            searchfield: ''
+        }
+    }
+componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then (response=> response.json())
+        .then(users => this.setState({ robots:users}));
+
+}
+
+onSearchChange = (event ) => {
+    this.setState({ searchfield:event.target.value })
+}
+
+    render(){
+        const filteredRobots = this.state.robots.filter( robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className = 'tc'>
+                <h1 className='f2'>Robofriends</h1>
+                <SearchBox searchChange = {this.onSearchChange} />
+            <Scroll>
+                <CardList robots = {filteredRobots} />
+            </Scroll>    
+            </div>
+        );
+    } 
+}
+
+
+export default App; 
